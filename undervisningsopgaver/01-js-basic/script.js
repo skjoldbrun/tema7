970539f81e5e0683ad3f57a1f88aed18dc60545c
”Louise <loui82m9@stud.kea.dk>
2022-01-31 21:14:03 +0100

    areal

---
+++ undervisningsopgaver/01-js-basic/script.js
@@ -0,0 +1,5 @@
+let længde = 3;
+let bredde = 5;
+let areal = længde * bredde;
+let resultat = `Længden er ${længde} meter og bredden er ${bredde} meter. Arealet er ${areal}`;
+console.log(resultat);
